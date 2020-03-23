df7d5f4cfb0e11c50cc288147c0e1aaebf6bb5cb
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-03-23 10:59:34 -0400

    Added a validateAction middleware

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -30,11 +30,33 @@ const validateProject = (req, res, next) => {
 		? res.status(400).json({ message: "missing project data" })
 		: !body.name || !body.description
 		? res.status(400).json({
-				message: `missing required ${!body.name ? "name" : "description"} field`
+				message: `missing required 
+        ${!body.name ? "name" : !body.description ? "description" : null} field`
 		  })
+		: next();
+};
+
+const validateAction = (req, res, next) => {
+	const { body } = req;
+
+	!body
+		? res.status(400).json({ message: "missing action data" })
+		: !body.project_id
+		? res.status(400).json({ message: "missing required project_id" })
+		: !body.description || !body.notes
+		? res.status(400).json({
+				message: `missing required 
+          ${
+						!body.description ? "description" : !body.notes ? "notes" : null
+					} field`
+		  })
+		: !body.description.length <= 128
+		? res
+				.status(400)
+				.json({ message: "description must be 128 characters or fewer" })
 		: next();
 };
 
 const middleware = [logger];
 
-module.exports = { middleware, validateId, validateProject };
+module.exports = { middleware, validateId, validateProject, validateAction };
