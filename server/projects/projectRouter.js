a399a530dc926e4f97da9c85b91382b3b2ed60ae
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-03-23 13:16:57 -0400

    Wrote first endpoint for projectRouter, Post request

--- server/projects/projectRouter.js
+++ server/projects/projectRouter.js
@@ -1,14 +1,35 @@
 const express = require("express");
 
 const projectDb = require("../../data/helpers/projectModel");
-const { validateId } = require("../middleware/middleware");
+const { validateId, validateProject } = require("../middleware/middleware");
 
 const router = express.Router();
 
 // Endpoints
 
+router.get("/", (req, res) => {
+	projectDb.get().then(projects => {
+		res.status(200).json(projects);
+	});
+});
+
 router.get("/:id", validateId(projectDb), (req, res) => {
 	res.status(200).json(req.item);
+});
+
+router.post("/", validateProject, (req, res) => {
+	const project = req.body;
+
+	projectDb
+		.insert(project)
+		.then(newProject => {
+			res.status(201).json(newProject);
+		})
+		.catch(error =>
+			res
+				.status(500)
+				.json({ message: "Error adding project to the database", error })
+		);
 });
 
 module.exports = router;
